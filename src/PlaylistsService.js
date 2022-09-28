f96a56b53627e87b5eb9411827bd7c7be58dadf9
Dias Utsman <utsmand91@gmail.com>
2022-09-28 07:40:52 +0700

    Fix indentations

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -29,9 +29,9 @@ class PlaylistsService {
 
     playlist.songs = (await this._pool.query({
       text: `SELECT songs.id, songs.title, songs.performer FROM songs
-                LEFT JOIN playlists_songs ON playlists_songs.song_id = songs.id
-                WHERE playlists_songs.playlist_id = $1
-                GROUP BY songs.id`,
+            LEFT JOIN playlists_songs ON playlists_songs.song_id = songs.id
+            WHERE playlists_songs.playlist_id = $1
+            GROUP BY songs.id`,
       values: [id],
     })).rows;
 
